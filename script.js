c5afface6c3a352ecaecf5120e6c94cfe31e1606
Brendan Ross <bross@zipcar.com>
2015-01-22 17:09:42 -0500

    Updated code with ability to add cells through clicks. Run/step/clear buttons now work as intended

--- script.js
+++ script.js
@@ -1,68 +1,106 @@
 $(function() {
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
+
+	runnerID = 0;
+	delay = 300; // ms
+	cellSize = 10; // pixels
+
 	resizeCanvas(); // Set canvas to correct size
+	life = new Life(cellSize);
 
-	cellSize = 5;
-    life = new Life(500, cellSize);
+	updateView();
 
-    updateGrid();
+	// -------------------------------------------------------------------
+
+	// BUTTON HANDLERS
+	$("#run").click(function() {
+		if(life.running) {
+			clearInterval(runnerID);
+			life.running = false;
+			setButtonText($("#run"), "Run")
+			$("#step").prop("disabled",false);
+		} else {
+			runnerID = window.setInterval(function(){
+				life.tick();
+				updateView();
+			}, delay);
+			life.running = true;
+			setButtonText($("#run"), "Stop")
+			$("#step").prop("disabled",true);
+		}
+	});
+	$("#step").click(function() {
+		life.tick();
+		updateView();
+	});
+	$("#clear").click(function() {
+		life.reset();
+		updateView();
+	});
+
+	// CANVAS CLICK HANDLER
+	$("#canvas").click(function(e) {
+		addNewCellFromClick(e);
+	});
 });
 
-Array.matrix = function (m, n, initial) {
+Array.matrix = function(m, n, initial) {
 	var a, i, j, mat = [];
-	for (i = 0; i < m; i += 1) {
+	for(i = 0; i < m; i++) {
 		a = [];
-		for (j = 0; j < n; j += 1) {
-			a[j] = 0;
+		for(j = 0; j < n; j++) {
+			a[j] = initial;
 		}
 		mat[i] = a;
 	}
 	return mat;
-};
+}
 
-// GAME OF LIFE LOGIC
-function Life(delay, cellsize) {
-	this.CELLSIZE = cellsize; // size of cell in pixels
-	this.x = canvas.width;
-	this.y = canvas.height;
-	this.WIDTH = this.x / this.CELLSIZE; // number of cells
-	this.HEIGHT = this.y / this.CELLSIZE; // number of cells
-	this.DELAY = delay; // Delay of a tick (milliseconds)
+// LOGIC
+function Life(cellSize) {
 	this.DEAD = 0;
 	this.ALIVE = 1;
 	this.running = false;
 
-	this.minimun = 2; // Minimum number of alive nieghbors to stay alive
-	this.maximum = 3; // Maximum number of alive neighbors to stay alive
-	this.spawn = 3; // Number of alive neighbors to bring a dead cell back to this
+	this.minimum = 2; // Minimum number of alive neighboring cells to stay alive
+	this.maximum = 3; // Maximum number of alive neighboring cells to stay alive
+	this.spawn = 3;   // Number of alive neighboring cells to revive a dead cell
 
-	this.grid = Array.matrix(this.HEIGHT, this.WIDTH, 0);
+	this.CELLSIZE = cellSize;
+	this.WIDTH = canvas.width / this.CELLSIZE;
+	this.HEIGHT = canvas.height / this.CELLSIZE;
 
+	this.grid = Array.matrix(this.HEIGHT, this.WIDTH, this.DEAD);
 	this.counter = 0;
+	this.aliveCount = 0;
 
 	this.tick = function() {
 		var neighborCount;
-		var nextGrid = Array.matrix(this.HEIGHT, this.WIDTH, 0);
+		var nextGrid = Array.matrix(this.HEIGHT, this.WIDTH, this.DEAD);
+		var aliveCountTemp = 0; 
 
 		for(y = 0; y < this.HEIGHT; y++) {
 			for(x = 0; x < this.WIDTH; x++) {
-				neighborCount = countNeighbors(y, x);
-				if(grid[y][x] === this.ALIVE) {
-					if(neighborCount > this.maximum 
-						&& neighborCount >= this.minimun) {
+				neighborCount = this.countNeighbors(y, x);
+				if(this.grid[y][x] === this.ALIVE) {
+					if(neighborCount >= this.minimum && neighborCount <= this.maximum) {
 						nextGrid[y][x] = this.ALIVE;
+						aliveCountTemp++;
 					}
 				} else {
-					if(neighborCount === Life.spawn) {
+					if(neighborCount === this.spawn) {
 						nextGrid[y][x] = this.ALIVE;
+						aliveCountTemp++;
 					}
 				}
 			}
 		}
+
 		this.copyGrid(nextGrid, this.grid);
+		this.aliveCount = aliveCountTemp;
 		this.counter++;
-	};
+	}
 
 	this.countNeighbors = function(y, x) {
 		var aliveNeighbors = (this.grid[y][x] === this.ALIVE) ? -1 : 0;
@@ -70,50 +108,76 @@ function Life(delay, cellsize) {
 			for(var col = -1; col <= 1; col++) {
 				var cell;
 				try {
-					if(this.grid[y + row][x + col] === this.ALIVE) {
-						aliveNeighbors++;
-					}
+					if(this.grid[y + row][x + col] === this.ALIVE) { aliveNeighbors++; }	
 				} catch(err) {}
 			}
 		}
-		return aliveNeighbors; 
-	};
+		return aliveNeighbors;
+	}
 
 	this.copyGrid = function(source, destination) {
 		for(var row = 0; row <= this.HEIGHT; row++) {
 			destination[row] = source[row].slice(0);
 		}
-	};
+	}
+
+	this.reset = function() {
+		this.grid = Array.matrix(this.HEIGHT, this.WIDTH, this.DEAD);
+		this.counter = 0;
+		this.aliveCount = 0;
+	}
 }
 
 // DRAWING
 function resizeCanvas() {
 	canvas.width = $('#world').width();
-	canvas.height = 350;
+	canvas.height = 400;
 
 	drawGrid();
+}
+
+function updateView() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawGrid();
+	updateGrid();
+	updateCounters();
 }
 
 function drawGrid() {
 	var width = canvas.width;
 	var height = canvas.height;
 
-	// draw horizontal lines
-	for(var x = 0; x <= width; x += cellSize) {
+	for(var x = 0; x < canvas.width; x+= cellSize) {
 		drawLine(x, 0, x, height);
 	}
-
-	// draw vertical lines
-	for(var y = 0; y <= height; y += cellSize) {
+	for(var y = 0; y < canvas.height; y+= cellSize) {
 		drawLine(0, y, width, y);
 	}
+}
+
+function updateGrid() {
+	for(var row = 0; row < life.HEIGHT; row++) {
+		for(var col = 0; col < life.WIDTH; col++) {
+			if(life.grid[row][col] === life.ALIVE) {
+				drawBlock(row, col);
+			}
+		}
+	}
+}
+
+function updateCounters() {
+	var genNumber = $('#genNumber');
+	var liveNumber = $('#liveNumber');
+
+	genNumber.text(life.counter);
+	liveNumber.text(life.aliveCount);
 }
 
 function drawLine(startX, startY, endX, endY) {
 	context.beginPath();
 	context.moveTo(startX, startY);
 	context.lineTo(endX, endY);
-	context.strokeStyle = '#C0C0C0';
+	context.strokeStyle = '#d3d3d3';
 	context.stroke();
 }
 
@@ -125,13 +189,47 @@ function drawBlock(y, x) {
 	context.stroke();
 }
 
-function updateGrid() {
-	for(var row = 0; row < life.HEIGHT; row++) {
-		for(var col = 0; col < life.WIDTH; col++) {
-			if(life.grid[row][col] === life.ALIVE) {
-				console.log("DRAW");
-				drawBlock(row, col);
-			}
-		}
+// CLICK LOCATION LOGIC
+
+function relMouseCoords(event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
+function addNewCellFromClick(event) {
+	var coords = canvas.relMouseCoords(event);
+	var cellX = Math.floor(coords.x / cellSize);
+	var cellY = Math.floor(coords.y / cellSize);
+	var status = life.grid[cellX][cellY];
+
+	console.log(cellY, cellX, status);
+
+	if(status) {
+		life.grid[cellY][cellX] = life.DEAD;
+		life.aliveCount -= 1;
+	} else {
+		life.grid[cellY][cellX] = life.ALIVE;
+		life.aliveCount += 1;
 	}
+	updateView();
+}
+
+// MISC
+function setButtonText($button, text) {
+	$button.text(text);
 }
